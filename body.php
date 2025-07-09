c06ecf06ab7bb7dd9c103a98594a62575d60638d
kouser-ahamed <kouserahamed.cse.diu@gmail.com>
2025-07-09 10:10:40 +0600

    Send Data to Server

--- body.php
+++ body.php
@@ -1,5 +1,6 @@
- <section>
-        <h1>
-            This is body of the page
-        </h1>
-    </section>
+<section>
+    <form action="server.php" method="POST">
+        <input type="text" id="vall" name="vall" placeholder="Enter text">
+        <button type="submit">Send Data to Server</button>
+    </form>
+</section>
