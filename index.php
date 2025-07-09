deeb4d2d6c3bf7b39b5d54e53d350f0dbda12e26
kouser-ahamed <kouserahamed.cse.diu@gmail.com>
2025-07-09 09:44:35 +0600

    Print 1 to 10

--- index.php
+++ index.php
@@ -1,21 +1,3 @@
-<!-- <?php
-
-$var1 = 5;
-$var2 = 30;
-$sum = $var1 + $var2;
-
-if($var1 > $var2){
-    echo "Var 1 is grater Value = " .$var1 . "<br>";
-}
-else if($var1 == $var2){
-    echo "Variable are equal";
-}
-else{
-   echo "Var 2 is grater Value = " .$var2 . "<br>";
-}
-
-?> -->
-
 <?php
 for ($i = 1; $i <= 10; $i++) {
     echo "This is line number $i<br>";
