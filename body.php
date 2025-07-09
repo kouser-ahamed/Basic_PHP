1e85f46035e92e0950f15d83667447592c0f766a
kouser-ahamed <kouserahamed.cse.diu@gmail.com>
2025-07-09 10:59:33 +0600

    Create Database and enter name or age and same To Database

--- body.php
+++ body.php
@@ -1,6 +1,7 @@
 <section>
-    <form action="server.php" method="POST">
-        <input type="text" id="val1" name="val1" placeholder="Enter text">
-        <button type="submit">Send Data to Server</button>
-    </form>
-</section>
+        <form action="server.php" method="POST">
+            <input type="text" id="val1" name="val1" placeholder="Enter Name">
+            <input type="number" id="val2" name="val2" placeholder="Enter Age">
+            <button type="submit">Save to DB</button>
+        </form>
+    </section>
--- server.php
+++ server.php
@@ -1,5 +1,29 @@
 <?php
+
+$username = "root";
+$password = "";
+$host = "localhost";
+$database = "dummy_db";
+
+$conn = mysqli_connect($host, $username, $password, $database);
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    echo $_POST["val1"]; 
+    $name = $_POST["val1"];
+    $age = $_POST["val2"];
+
+    $sql_query = "INSERT INTO users (name, age) VALUES ('$name','$age')";
+    $conn->query($sql_query);
+
+    echo "Data Saved Successfully";
 }
-?>
+
+// <!--CREATE DATABASE IF NOT EXISTS dummy_db;
+
+// USE dummy_db;
+
+// CREATE TABLE users (
+//     id INT AUTO_INCREMENT PRIMARY KEY,
+//     name VARCHAR(100),
+//     age INT
+// );
+// -->
